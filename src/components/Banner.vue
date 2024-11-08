4b76c3c7789596beb24729501ca8f69bee804d20
Bikeev Erkanat <EBikeev@beeline.kz>
2024-11-08 17:13:02 +0500

    s

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,14 @@
 <script setup>
 //из banner app.vue
-const { } =defineProps(["banner"])
+const {banner} =defineProps(["banner"])
+const {
+    title,
+    overview,
+    bacdrop_path,
+    release_date,
+    vote_average,
+    original_language
+} = banner
 
 //console.log(banner)
 </script>
@@ -9,4 +17,5 @@ const { } =defineProps(["banner"])
     <!-- <h1 style="background-color: aliceblue;">HelloWorld111</h1> -->
      <h1>ff</h1>
     <p>{{ banner }}</p>
+    <p>{{ banner.title}}</p>
 </template>
